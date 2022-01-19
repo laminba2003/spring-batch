e735a4e7bf71ec077e003d67e58a000304c41088
mamadou lamine ba <laminba2003@gmail.com>
2022-01-19 18:45:37 +0000

    initial commit

---
+++ src/test/java/com/spring/trainging/batch/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.trainging.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
