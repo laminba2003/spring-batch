c1153e546e5d05f2ca383c10851f56093fbcbe44
mamadou lamine ba <laminba2003@gmail.com>
2022-01-19 18:51:29 +0000

    initial commit

--- src/main/java/com/spring/trainging/batch/PersonItemProcessor.java
+++ src/main/java/com/spring/trainging/batch/PersonItemProcessor.java
@@ -2,7 +2,6 @@ package com.spring.trainging.batch;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.batch.item.ItemProcessor;
 
 public class PersonItemProcessor implements ItemProcessor<Person, Person> {
